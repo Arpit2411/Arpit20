e47622aefd3cd593a0099c4b495c6f559936521b
Arpit2411 <arpitbaderiya98@gmail.com>
2018-03-31 19:50:14 +0530

    condition check
    condition check for safe and unsafe state

--- 20.c
+++ 20.c
@@ -15,14 +15,30 @@ int main()
 	scanf("%d",&q);
 	resource=p;
 	int process[q],finish[q],allot[q];
+	int process[q],finish[q],allot[q];
 	for(i=0;i<q;i++)
 	{
 		printf("Enter number of resources required by Process %d\n",i+1);
 		scanf("%d",&process[i]);
+		finish[i]=0;
+		allot[i]=0;
 	}
 	printf("Process \tResources\n");
 	for(i=0;i<q;i++)
 	{
 		printf("P%d \t\t%d\n",i+1,process[i]);
+		max_need=max_need+process[i];
+		if(process[i]<1||process[i]>p)
+		{
+			count++;
+		}
+	}
+	if(max_need<p+q&&count==0)
+	{	
+		printf("Both Condition i.e \n1)resource need of each process is between 1 and p \n2)Max need is less than p+q\nwhere q is no. of process and p is no. of resource is satisfied\nSo surely system is in safe state there is no chance of deadLock.\n");
+	}
+	else
+	{
+		printf("Conditions are not satisfied so system is in unsafe state,there are chances of dead lock\n");
 	}
 }
